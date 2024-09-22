9107e2c8ea8a3c5ed74e22d848b1168f63ea3a33
daophanquochoai <hoai23828@mgail.com>
2024-09-22 17:37:40 +0700

    init

---
+++ prduct-commmand-service/src/main/java/com/doctorhoai/prductcommmandservice/PrductCommmandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doctorhoai.prductcommmandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrductCommmandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrductCommmandServiceApplication.class, args);
+	}
+
+}
---
+++ prduct-commmand-service/src/test/java/com/doctorhoai/prductcommmandservice/PrductCommmandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorhoai.prductcommmandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrductCommmandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
